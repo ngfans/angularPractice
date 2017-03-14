7d4b1e65ce729878660b1a834381c43343664a9d
ngfans <ybbwfnta@126.com>
2017-03-14 22:16:27 +0800

    上传

---
+++ app2/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { App2Page } from './app.po';
+
+describe('app2 App', () => {
+  let page: App2Page;
+
+  beforeEach(() => {
+    page = new App2Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
